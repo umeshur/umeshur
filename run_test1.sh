7aea18d03e1d364ed77b28ede1dc57e4dbd192b6
umeshur <97522664+umeshur@users.noreply.github.com>
2023-08-19 13:45:27 +0530

    Update run_test1.sh

--- run_test1.sh
+++ run_test1.sh
@@ -9,7 +9,7 @@ echo "#################################################$now#####################
 echo ""  #Just empty line
 #file with team and repo names with permissions
 TXT_FILE="repolist.txt"   #API has diffrent names for Read and Write. API Read is passed in as pull & Write is push
-#Loop through CSV entries and change permissions
+#Loop through TXT entries and change permissions
 while IFS=$',' read -r team repo permission
 do
 curl -L -X PUT -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $PAT" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/WoolworthsSOISaml/teams/$team/repos/WoolworthsSOISaml/$repo -d '{"permission":"'$permission'"}'
@@ -26,4 +26,4 @@ else
         echo "---------------------------------------------------------------------------------------" >> $output
 fi
 done < "$TXT_FILE"
-echo "Task is complted and output stored at $output."
+echo "Task is completed and result is saved at $output."
